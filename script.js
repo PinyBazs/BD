95e446be9c5e9d954a1773de35756ae0660f9e95
PinyBazs <68302547+PinyBazs@users.noreply.github.com>
2025-02-06 17:45:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-let percent = 0;
+let percent = 100;
 let interv;
 function OpenGift() {
     let gift = document.getElementById("gift-wrapper");
     if(gift){
         gift.remove();
-        interv = setInterval(ShowTitle, 100);
+        interv = setInterval(HideConfetti, 100);
     }
         
   }
@@ -13,11 +13,12 @@ function OpenGift() {
 function ShowTitle() {
 
     let title = document.getElementById("concert-title");
+
     if(title){
         
         percent = percent + 5;
         title.style.opacity = String(percent + "%");
-
+        console.log("tit" + percent);
         if(title.style.opacity == 1 ){
             console.log("stop");
             clearInterval(interv);
@@ -26,3 +27,22 @@ function ShowTitle() {
     }
         
   }
+
+function HideConfetti() {
+
+    let confetti = document.getElementById("confetti");
+
+    if(confetti){
+        
+        percent = percent - 5;
+        confetti.style.opacity = String(percent + "%");
+        console.log("con" + percent);
+        if(confetti.style.opacity == 0 ){
+            clearInterval(interv);
+            interv = setInterval(ShowTitle, 100);
+
+        }
+        
+    }
+        
+}
